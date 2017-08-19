65931190c5b948655e87e88eff1a6893cb19447b
chaitanyavp <chaitanya1998@gmail.com>
2017-08-18 20:47:43 -0400

    Add construct for getting binge time of anime by id

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,15 @@
-window.onload = function(){
-}
+// window.onload = function(){
+	// const bad = document.getElementById("bad");
+	// bad.addEventListener('click', showAPI);
+// }
+
+// function showAPI(){
+// 	document.getElementById("api").style.display = "";
+// }
+
+$(function() {
+  $('a#show_whatever').click(function(event){
+    event.preventDefault();
+    $('div#whatever').toggle();
+  });
+});
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ require 'json'
 class PagesController < ApplicationController
     def home
     end
+    def to_s
+	  attributes.each_with_object("") do |attribute, result|
+	    result << "#{attribute[1].to_s} "
+		end
+	end
+
 end
